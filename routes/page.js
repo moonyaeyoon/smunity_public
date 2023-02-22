bfbd6e7c83f4e0b81451d27f96029a6d9ba8f400
sunjungAn <dkstjswjd83@naver.com>
2023-02-22 15:46:40 +0900

    feat: :tada: node.js init setting

---
+++ routes/page.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+    res.locals.user = null;
+    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
+    res.locals.followerIdList = [];
+    next();
+});
+
+
+router.get('/board', (req, res)=>{
+    res.send('게시판')
+})
+
+router.get('/', (req, res, next) => {
+    res.send('랜딩페이지')
+    //res.render('/', {title: "랜딩 페이지"});
+})
+
+module.exports= router;
