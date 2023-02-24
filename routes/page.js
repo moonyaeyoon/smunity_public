4a2a72a0c2b4f0e68625d5e832487c50f2eec0e2
newoldname <thjbjs@outlook.com>
2023-02-25 03:21:59 +0900

    db연결 성공, 간단한 읽기/쓰기 성공

--- routes/page.js
+++ routes/page.js
@@ -1,22 +1,38 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
+const { sequelize } = require("../models");
+const POST = require("../models/post");
+const USER = require("../models/user");
+
 router.use((req, res, next) => {
-    res.locals.user = null;
-    res.locals.followerCount = 0;
-    res.locals.followingCount = 0;
-    res.locals.followerIdList = [];
-    next();
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
 });
 
-router.get('/board', (req, res)=>{
-    res.send('게시판')
-})
+router.get("/board", async (req, res) => {
+  const result = await POST.findAll();
+  res.send(result);
+});
 
-router.get('/', (req, res, next) => {
-    res.send('랜딩페이지')
-    //res.render('/', {title: "랜딩 페이지"});
-})
+router.post("/board/new", async (req, res) => {
+  const { userId, title, content, majorId } = req.body.params;
+  const result = await POST.create({
+    title: title,
+    content: content,
+    UserId: userId,
+    MajorId: majorId,
+  });
+  res.send(result);
+});
 
-module.exports= router;
+router.get("/", (req, res, next) => {
+  res.send("랜딩페이지");
+  //res.render('/', {title: "랜딩 페이지"});
+});
+
+module.exports = router;
