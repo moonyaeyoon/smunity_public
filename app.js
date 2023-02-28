0eb80a024e928bef4ca1f7b90f9a089c132a0adb
newoldname <thjbjs@outlook.com>
2023-03-01 01:01:15 +0900

    cancel

--- app.js
+++ app.js
@@ -24,63 +24,63 @@ nunjucks.configure('views', {
 sequelize.sync({ force : true}).then(async() => {
     console.log('데이터베이스 연결 성공');
     //TODO: 나중에 rds로 db통합되면 이 쿼리는 없어집니다. local에서 자동으로 생성되도록 합니다. 최초 실행 이후에, 이 코드는 지워주세요.
-    // Major.create({majorName: "컴퓨터과학과"}) // 001
-    // Major.create({majorName: '휴먼지능정보공학전공'}); // 002
-    // Major.create({majorName: '경제학과'}); // 003
+    Major.create({majorName: "컴퓨터과학과"}) // 001
+    Major.create({majorName: '휴먼지능정보공학전공'}); // 002
+    Major.create({majorName: '경제학과'}); // 003
     
     //자유 게시판: 001, 비밀 게시판: 002, 공지게시판: 003
 
-    // Board.create({
-    //     boardId: "001001",
-    //     boardName: "CS Free Board",
-    //     isCanAnonymous: false,
-    //     isFree: true,
-    //     MajorId: 1
-    // });
-    // Board.create({
-    //     boardId: "001002",
-    //     boardName: "CS Secret Board",
-    //     isCanAnonymous: true,
-    //     isFree: true,
-    //     MajorId: 1
-    // });
-    // Board.create({
-    //     boardId: "001003",
-    //     boardName: "CS Noti Board",
-    //     isCanAnonymous: false,
-    //     isFree: false,
-    //     MajorId: 1
-    // });
-    // Board.create({
-    //     boardId: "002001",
-    //     boardName: "Human Free Board",
-    //     isCanAnonymous: false,
-    //     isFree: true,
-    //     MajorId: 2
-    // });
+    Board.create({
+        boardId: "001001",
+        boardName: "CS Free Board",
+        isCanAnonymous: false,
+        isFree: true,
+        MajorId: 1
+    });
+    Board.create({
+        boardId: "001002",
+        boardName: "CS Secret Board",
+        isCanAnonymous: true,
+        isFree: true,
+        MajorId: 1
+    });
+    Board.create({
+        boardId: "001003",
+        boardName: "CS Noti Board",
+        isCanAnonymous: false,
+        isFree: false,
+        MajorId: 1
+    });
+    Board.create({
+        boardId: "002001",
+        boardName: "Human Free Board",
+        isCanAnonymous: false,
+        isFree: true,
+        MajorId: 2
+    });
 
-    // const user1 = await User.create({
-    //     email: "first@first.com",
-    //     nick: "firstNick",
-    //     password: "firstPW",
-    //     provider: "local",
-    // });
-    // user1.addMajor(1);
-    // user1.addBoard("001001")
-    // user1.addBoard("001002")
-    // user1.addMajor(2);
-    // user1.addBoard("002001")
+    const user1 = await User.create({
+        email: "first@first.com",
+        nick: "firstNick",
+        password: "firstPW",
+        provider: "local",
+    });
+    user1.addMajor(1);
+    user1.addBoard("001001")
+    user1.addBoard("001002")
+    user1.addMajor(2);
+    user1.addBoard("002001")
 
-    // const user2 = await User.create({
-    //     email: "second@second.com",
-    //     nick: "secondNick",
-    //     password: "secondPW",
-    //     provider: "local",
-    // })
-    // user2.addMajor(1)
-    // user2.addBoard("001001")
-    // user2.addBoard("001002")
-    // user2.addBoard("001003") //컴과 공지 게시판 쌉가능
+    const user2 = await User.create({
+        email: "second@second.com",
+        nick: "secondNick",
+        password: "secondPW",
+        provider: "local",
+    })
+    user2.addMajor(1)
+    user2.addBoard("001001")
+    user2.addBoard("001002")
+    user2.addBoard("001003") //컴과 공지 게시판 쌉가능
 
 }).catch((err) => {
     console.error(err);
