c4112e08aab666538120bbb4097a9bfe48a07fb5
sunjungAn <dkstjswjd83@naver.com>
2023-03-22 16:12:58 +0900

    feat: 인증 관련 함수

--- routes/manage.js
+++ routes/manage.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { verifyAToken } = require('../middlewares');
-const { getCertificateList, basic, addCertificate } = require('../services/manage/certificate');
+const { getCertificateList, basic, addCertificate, updateMajorInCertificate } = require('../services/manage/certificate');
 
 const router = express.Router();
 
 router.get('/certificate/list', getCertificateList);
 router.post('/certificate/create', verifyAToken, addCertificate);
+router.put('/certificate/update', updateMajorInCertificate);
 module.exports = router;
--- services/manage/certificate.js
+++ services/manage/certificate.js
@@ -1,11 +1,12 @@
-const { MajorAuthPost } = require('../../models');
+const { MajorAuthPost, UserMajors } = require('../../models');
 
 exports.getCertificateList = async (req, res, next) => {
     try {
-        const certificateList = await MajorAuthPost.findAll({ where: { deletedAt: null } });
+        const certificateList = await MajorAuthPost.findAll();
         res.json({ code: 200, payload: certificateList });
     } catch (err) {
         console.error(err);
+        next(err);
     }
 };
 
@@ -13,14 +14,51 @@ exports.addCertificate = async (req, res, next) => {
     try {
         const USER_ID = res.locals.decodes.user_id;
         const { imageUrl, content } = req.body;
+        if (!imageUrl || !content) {
+            return res.status(RES_ERROR_JSON.REQ_FORM_ERROR.status_code).json(RES_ERROR_JSON.REQ_FORM_ERROR.res_json);
+        }
         await MajorAuthPost.create({
-            UserId: USER_ID,
-            imageUrl: imageUrl,
+            user_id: USER_ID,
+            image_url: imageUrl,
             content: content,
         });
         res.json({ code: 200, payload: '인증 요청 성공' });
     } catch (err) {
         console.error(err);
         next(err);
+    }
+};
+
+exports.updateMajorInCertificate = async (req, res, next) => {
+    const { userId, majorList } = req.body;
+    if (!userId || !majorList) {
+        return res.status(RES_ERROR_JSON.REQ_FORM_ERROR.status_code).json(RES_ERROR_JSON.REQ_FORM_ERROR.res_json);
+    }
+    const isUserMajorsDeleted = await deleteUserMajorList(userId);
+    try {
+        if (isUserMajorsDeleted) {
+            majorList.forEach(async (element) => {
+                await UserMajors.create({
+                    user_id: userId,
+                    major_id: element,
+                });
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+};
+
+const deleteUserMajorList = async (userId) => {
+    try {
+        await UserMajors.destroy({
+            where: { user_id: userId },
+        });
+        return true;
+    } catch (err) {
+        console.err(err);
+        next(err);
+        return false;
     }
 };
