7489d45394e863aa65f85e03e9afea63eb4fd8a6
Minju0811 <95904729+Minju0811@users.noreply.github.com>
2023-03-28 00:44:05 +0900

    S3 업로드 api작성
    임시적으로 url/upload 에 post요청을 보내면 되도록 코드를 짰어요 요청 body에 폼데이터로 image/파일을 보내면 정상적으로 업로드 되고 미들웨어로 사용할지 따로 요청을 보낼지는 상의해보는게 좋을 것같습니다

--- routes/page.js
+++ routes/page.js
@@ -7,6 +7,7 @@ const ManageRouter = require('./manage');
 const BoardRouter = require('./board');
 const AuthRouter = require('./auth');
 const CommentRouter = require('./comment');
+const ImageRouter = require('./upload');
 
 router.use((req, res, next) => {
     res.locals.user = req.user;
@@ -18,6 +19,7 @@ router.use('/board', BoardRouter);
 router.use('/comment', CommentRouter);
 
 router.use('/auth', AuthRouter);
+router.use('/upload', ImageRouter);
 
 router.get('/', (req, res, next) => {
     res.send('랜딩페이지');
