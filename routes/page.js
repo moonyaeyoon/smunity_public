b33e0c5ab5cc6d8dd36fd24809db415a35d927db
sunjungAn <dkstjswjd83@naver.com>
2023-03-30 00:31:29 +0900

    feat: ses setting

--- routes/page.js
+++ routes/page.js
@@ -6,6 +6,7 @@ const { sequelize } = require('../models');
 const ManageRouter = require('./manage');
 const BoardRouter = require('./board');
 const AuthRouter = require('./auth');
+const mailRouter = require('./mail');
 
 router.use((req, res, next) => {
     res.locals.user = req.user;
@@ -16,7 +17,7 @@ router.use('/manage', ManageRouter);
 router.use('/board', BoardRouter);
 
 router.use('/auth', AuthRouter);
-
+router.use('/mail', mailRouter);
 router.get('/', (req, res, next) => {
     res.send('랜딩페이지');
     //res.render('/', {title: "랜딩 페이지"});
