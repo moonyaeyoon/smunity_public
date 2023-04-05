702a773c08d1d2d235fceb75bbc78e60ead526e0
newoldname <thjbjs@outlook.com>
2023-04-05 14:58:34 +0900

    크롤링 스케줄링 부분 추가

--- crawling/school_notice.py
+++ crawling/school_notice.py
@@ -1,7 +1,10 @@
 # Python 3.8.10 - 64-bit
+
 # pip install beautifulsoup4
 # pip install requests
 # pip install pymongo
+# pip install schedule
+
 # DB - database name: school_notice
 # DB - collection name: seoul_new_100
 
@@ -9,31 +12,41 @@ import requests
 from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+import schedule
+import time
 
-noticeDB = client["school_notice"]
-noticeTable = noticeDB["seoul_new_100"]
+def getSchoolNotice(): 
+    client = MongoClient("mongodb://localhost:27017/")
+    noticeDB = client["school_notice"]
+    noticeTable = noticeDB["seoul_new_100"]
 
-campus = "smu" #천안캠은 smuc
-page = requests.get(f'https://www.smu.ac.kr/lounge/notice/notice.do?srUpperNoticeYn=on&srCampus={campus}&article.offset=0&articleLimit=100')
+    campus = "smu" #천안캠은 smuc
+    page = requests.get(f'https://www.smu.ac.kr/lounge/notice/notice.do?srUpperNoticeYn=on&srCampus={campus}&article.offset=0&articleLimit=100')
 
-soup = bs(page.content, "html.parser")
+    soup = bs(page.content, "html.parser")
 
-table_tag = soup.find('ul', class_="board-thumb-wrap")
-noticeList = table_tag.find_all("dl")
+    table_tag = soup.find('ul', class_="board-thumb-wrap")
+    noticeList = table_tag.find_all("dl")
 
-noticeTable.drop()
+    noticeTable.drop()
 
-for i in noticeList:
-    NOW_TITLE = i.dt.table.tbody.find_all("td")[2].text.replace("\t", "").replace("\r", "").replace("\n", "")
-    NOW_INDEX = i.dd.ul.find_all('li')[0].text.replace("\t", "").replace("\r", "").replace("\n", "").replace("No.", "")
-    # NOW_AUTHOR = i.dd.ul.find_all('li')[1].text.replace("\t", "").replace("\r", "").replace("\n", "").replace("작성자", "")
-    NOW_DATE = i.dd.ul.find_all('li')[2].text.replace("\t", "").replace("\r", "").replace("\n", "").replace("작성일", "")
-    NOW_VIEWS = i.dd.ul.find_all('li')[3].text.replace("\t", "").replace("\r", "").replace("\n", "").replace("조회수", "")
-    noticeTable.insert_one({
-        "title": NOW_TITLE,
-        "index": NOW_INDEX,
-        "date": NOW_DATE,
-        "views": NOW_VIEWS
-    })
+    for i in noticeList:
+        NOW_TITLE = i.dt.table.tbody.find_all("td")[2].text.replace("\t", "").replace("\r", "").replace("\n", "")
+        NOW_INDEX = i.dd.ul.find_all('li')[0].text.replace("\t", "").replace("\r", "").replace("\n", "").replace("No.", "")
+        # NOW_AUTHOR = i.dd.ul.find_all('li')[1].text.replace("\t", "").replace("\r", "").replace("\n", "").replace("작성자", "")
+        NOW_DATE = i.dd.ul.find_all('li')[2].text.replace("\t", "").replace("\r", "").replace("\n", "").replace("작성일", "")
+        NOW_VIEWS = i.dd.ul.find_all('li')[3].text.replace("\t", "").replace("\r", "").replace("\n", "").replace("조회수", "")
+        noticeTable.insert_one({
+            "title": NOW_TITLE,
+            "index": NOW_INDEX,
+            "date": NOW_DATE,
+            "views": NOW_VIEWS
+        })
+    print("Crawling Done")
     
+if __name__ == "__main__":
+    getSchoolNotice()
+    schedule.every().hour.do(getSchoolNotice)
+    while True:
+            schedule.run_pending()
+            time.sleep(1)
