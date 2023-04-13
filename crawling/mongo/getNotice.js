de7f70a1fc0e1ac833856047832ed38919781384
newoldname <thjbjs@outlook.com>
2023-04-13 14:54:02 +0900

    [fix] 크롤링 주소 수정됨 -> GCP사용

--- crawling/mongo/getNotice.js
+++ crawling/mongo/getNotice.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = `mongodb://${process.env.MONGODB_IP}:27017`;
 const client = new MongoClient(url);
 
 // Database Name
