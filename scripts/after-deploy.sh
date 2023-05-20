3b61a9e9d922782e5bec4e2d9179d0bdcc966986
Minju0811 <95904729+Minju0811@users.noreply.github.com>
2023-05-20 21:51:17 +0900

    cd 설정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,7 +3,7 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
-npm i
-pm2 start dist
+/home/ubuntu/.nvm/versions/node/v18.16.0/bin/npm i
+/usr/bin/pm2 /home/ubuntu/.nvm/versions/node/v18.16.0/bin/pm2 start dist
 
 #nvm으로 node를 설치하면 직접 경로를 지정해야함.
