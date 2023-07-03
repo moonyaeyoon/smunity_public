3c8b65a962aee12fb70604ac6ea9b2f7c46de283
minjumost <95904729+minjumost@users.noreply.github.com>
2023-07-04 01:17:51 +0900

    에러 로그 테스트

--- services/image/ImageUploader.js
+++ services/image/ImageUploader.js
@@ -31,7 +31,7 @@ exports.imageUploader = multer({
         acl: 'public-read-write',
     }),
     limits: {
-        fileSize: 1024 * 1024 * 5, // 5MB 이하로 제한 (원하는 크기로 수정)
+        fileSize: 1024 * 1024 * 1, // 5MB 이하로 제한 (원하는 크기로 수정)
     },
 });
 
