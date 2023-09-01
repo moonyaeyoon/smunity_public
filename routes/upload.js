72a41385255523b9ed3e156d869cd5d418a71c61
minjumost <95904729+minjumost@users.noreply.github.com>
2023-09-01 16:36:00 +0900

    s3 디렉터리 전략 완료

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const { imageUploader, imageRemover, getEmoticon } = require('../services/image/ImageUploader'); // 이미지 업로드 미들웨어 모듈
+const { profileImageUploader, postImageUploader, timetableImageUploader, imageRemover, getEmoticon } = require('../services/image/ImageUploader'); // 이미지 업로드 미들웨어 모듈
 const RES_ERROR_JSON = require('../constants/resErrorJson');
+const {verifyAToken} = require('../middlewares');
+const User = require('../models/user');
 
-router.post('/', imageUploader.array('image'), (req, res) => {
+
+const checkUserExistByUserId = async (userId) => {
+    const REQ_USER = await User.findOne({
+        where: {
+            id: userId,
+        },
+    });
+    //사용자 미존재
+    if (REQ_USER === null) return false;
+    else return REQ_USER;
+};
+
+
+router.post('/profile', verifyAToken, profileImageUploader.array('image'), (req, res) => {
+    if (!req.files || req.files.length === 0) {
+        return res.status(RES_ERROR_JSON.NO_IMAGE.status_code).json(RES_ERROR_JSON.NO_IMAGE.res_json);
+    }
+
+    const imageUrl = req.files.map((file) => file.location);
+    const imageUrls = imageUrl.join(',');
+    res.json({ imageUrls });
+});
+
+router.post('/post', verifyAToken, postImageUploader.array('image'), (req, res) => {
+    if (!req.files || req.files.length === 0) {
+        return res.status(RES_ERROR_JSON.NO_IMAGE.status_code).json(RES_ERROR_JSON.NO_IMAGE.res_json);
+    }
+
+    const imageUrl = req.files.map((file) => file.location);
+    const imageUrls = imageUrl.join(',');
+    res.json({ imageUrls });
+});
+
+router.post('/timetable', verifyAToken, timetableImageUploader.array('image'), (req, res) => {
     if (!req.files || req.files.length === 0) {
         return res.status(RES_ERROR_JSON.NO_IMAGE.status_code).json(RES_ERROR_JSON.NO_IMAGE.res_json);
     }
--- services/image/ImageUploader.js
+++ services/image/ImageUploader.js
@@ -4,8 +4,9 @@ const multerS3 = require('multer-s3');
 const path = require('path');
 const { DELETE_S3 } = require('../../constants/resSuccessJson');
 const { NO_IMAGE } = require('../../constants/resErrorJson');
+const { verifyAToken } = require('../../middlewares');
 const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp', '.PNG'];
-
+const jwt = require('jsonwebtoken');
 //aws 설정 및 s3객체 생성 함수
 const s3 = () => {
     aws.config.update({
@@ -17,16 +18,55 @@ const s3 = () => {
 };
 
 //이미지 업로드 함수
-exports.imageUploader = multer({
+exports.profileImageUploader = multer({
     storage: multerS3({
         s3: s3(),
         bucket: process.env.S3_BUCKET,
         key: (req, file, callback) => {
+            const userId = jwt.verify(req.headers.authorization, process.env.JWT_SECRET).user_id;
             const extension = path.extname(file.originalname);
             if (!allowedExtensions.includes(extension)) {
                 return callback(new Error('잘못된 형식의 파일입니다.')); //이미지파일이 아닌경우 에러
             }
-            callback(null, `userProfile/${Date.now()}_${file.originalname}`); //s3내 저장될 경로 설정하기!!
+            callback(null, `profile/${userId}/${Date.now()}${extension}`); //s3내 저장될 경로 설정하기!!
+        },
+        acl: 'public-read-write',
+    }),
+    limits: {
+        fileSize: 1024 * 1024 * 5, // 5MB 이하로 제한 (원하는 크기로 수정)
+    },
+});
+
+exports.postImageUploader = multer({
+    storage: multerS3({
+        s3: s3(),
+        bucket: process.env.S3_BUCKET,
+        key: (req, file, callback) => {
+            const userId = jwt.verify(req.headers.authorization, process.env.JWT_SECRET).user_id;
+            const extension = path.extname(file.originalname);
+            if (!allowedExtensions.includes(extension)) {
+                return callback(new Error('잘못된 형식의 파일입니다.')); //이미지파일이 아닌경우 에러
+            }
+            callback(null, `post/${userId}/${Date.now()}${extension}`); //s3내 저장될 경로 설정하기!!
+        },
+        acl: 'public-read-write',
+    }),
+    limits: {
+        fileSize: 1024 * 1024 * 5, // 5MB 이하로 제한 (원하는 크기로 수정)
+    },
+});
+
+exports.timetableImageUploader = multer({
+    storage: multerS3({
+        s3: s3(),
+        bucket: process.env.S3_BUCKET,
+        key: (req, file, callback) => {
+            const userId = jwt.verify(req.headers.authorization, process.env.JWT_SECRET).user_id;
+            const extension = path.extname(file.originalname);
+            if (!allowedExtensions.includes(extension)) {
+                return callback(new Error('잘못된 형식의 파일입니다.')); //이미지파일이 아닌경우 에러
+            }
+            callback(null, `timetable/${userId}/${Date.now()}${extension}`); //s3내 저장될 경로 설정하기!!
         },
         acl: 'public-read-write',
     }),
