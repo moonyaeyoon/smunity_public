a50db513a2da3949181104f6321977f43d8b2626
newoldname <thjbjs@outlook.com>
2023-09-03 02:13:36 +0900

    [feat] 이메일 플랫폼 Daum 스마트워크로 바꾸고, 이메일 관련 로직 모두 수정

---
+++ util/email.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer');
+
+const ADMIN1_PRIVATE_KEY = process.env.ADMIN1_PRIVATE_KEY;
+const DAUM_SMTP_PASSWORD = process.env.DAUM_SMTP_PASSWORD;
+const ADMIN_EMAIL_SETTING = {
+    host: 'smtp.daum.net',
+    port: 465,
+    secure: true,
+    dkim: {
+        domainName: 'smus.co.kr',
+        keySelector: 'admin1',
+        privateKey: ADMIN1_PRIVATE_KEY,
+    },
+    auth: {
+        user: 'admin@smus.co.kr',
+        pass: DAUM_SMTP_PASSWORD,
+    },
+};
+
+//학번+제목+이메일의 HTML내용으로 이메일을 보낸다.
+exports.sendEmailUseSchoolId = (school_id, subject, htmlContent) => {
+    const emailTo = school_id + '@sangmyung.kr';
+    try {
+        const transporter = nodemailer.createTransport(ADMIN_EMAIL_SETTING);
+        transporter.sendMail(
+            {
+                from: 'SMUS - 스뮤즈 관리자 계정 <admin@smus.co.kr>',
+                to: emailTo,
+                subject: subject,
+                html: htmlContent,
+            },
+            (error, info) => {
+                if (error) {
+                    throw new Error('Send Email Callback Error: ', error);
+                }
+            }
+        );
+    } catch (error) {
+        console.error('Send Email Error: ', error);
+    }
+};
