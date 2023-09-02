1986bfdb92d8e2c0b88bce6ea369d0e7ca68eea6
minjumost <95904729+minjumost@users.noreply.github.com>
2023-09-03 01:04:21 +0900

    믹스패널 사용자 트래킹 미들웨어 추가

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,12 @@
 const { rateLimit } = require('express-rate-limit');
 const jwt = require('jsonwebtoken');
 const { JWT_TOKEN_EXPIRED, JWT_TOKEN_WRONG, JWT_TOKEN_NOT_FOUND } = require('../constants/resErrorJson');
+const mixpanel = require('mixpanel');
+const User = require('../models/user');
+const UserMajor = require('../models/UserMajor');
+const Major = require('../models/major');
+
+const mixpanelClient = mixpanel.init(process.env.MIXPANEL_PROJECT_TOKEN);
 
 exports.verifyAToken = (req, res, next) => {
     if (!req.headers.authorization) return res.status(JWT_TOKEN_NOT_FOUND.status_code).json(JWT_TOKEN_NOT_FOUND.res_json);
@@ -33,3 +39,33 @@ exports.apiLimiter = rateLimit({
         });
     },
 });
+
+exports.trackEvent = async(req, res, next) => {
+    //Mixpanel
+    
+    const api = req.originalUrl;
+    const userID = res.locals.decodes.user_id;
+    
+    const NOW_USER = await User.findOne({ where : { id: userID}});
+    const NOW_USER_MAJOR_LIST = await UserMajor.findAll({ where: { user_id: NOW_USER.id } });
+
+    let major_list = "";
+    for(let i =0; i < NOW_USER_MAJOR_LIST.length; i++){
+        const major_id = NOW_USER_MAJOR_LIST[i].major_id;
+        const major = await Major.findOne({ where: { id: major_id }});
+        major_list += major.major_name + " ";
+    }
+    const school_id = NOW_USER.school_id;
+    const name = NOW_USER.nickname;
+
+    mixpanelClient.people.set(school_id, {
+        Nickname: name,
+        Major: major_list
+    });
+
+    
+    mixpanelClient.track(api, {
+        distinct_id: school_id
+    });
+    next();
+};
